60c038f4a53e1be6b353062c1da01b1bc6af1b15
UDanV <danil17092006@gmail.com>
2026-04-16 21:49:33 +0300

    update NEXTAUTH_URL in docker-compose, remove ecosystem.config.cjs, and adjust deployment workflow

--- app/shared/components/landing/cta/index.tsx
+++ app/shared/components/landing/cta/index.tsx
@@ -10,7 +10,7 @@ export const FinalCTA = () => {
 
   return (
     <>
-      {session?.user && (
+      {!session?.user && (
         <section id="cta" className="py-20 md:py-28">
           <div className="container mx-auto px-4 md:px-6">
             <motion.div
